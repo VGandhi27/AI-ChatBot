146286ecc66b327c2a8caf6fe88f7bf527f332cf
VGiktara27 <curiousvidushi27@gmail.com>
2025-04-27 22:08:13 +0530

    Update ChatEmbed.py

--- chatbot/src/ChatEmbed.py
+++ chatbot/src/ChatEmbed.py
@@ -11,7 +11,12 @@
 
 # Import Modules 
 import requests
+import logging
 from chatbot.src import Common
+
+'''***************************************** Main Code ********************************************'''
+# Adding app_logger
+chat_logger = logging.getLogger('app_logger')
 
 # Model Config 
 model_config =  Common.model_config()
@@ -21,6 +26,7 @@ OLLAMA_API_URL = f"http://{model_config['Host']}:{model_config['Port']}/api/embe
 
 # 🔹 Generate Embedding
 def chatem_generate_embedding(text, model=model_config['Model']):
+    chat_logger.debug (f'chatem_generate_embedding request: {text, model}')
     url = OLLAMA_API_URL
     payload = {
         "model": model,
@@ -32,8 +38,14 @@ def chatem_generate_embedding(text, model=model_config['Model']):
         result = response.json()
         embeddings = result.get("embedding", [])
         if embeddings:
+            chat_logger.debug (f'Embeddings generated')
             return embeddings
         else:
+            chat_logger.debug (f'No embeddings returned.')
+            chat_logger.error (f'No embeddings returned.')
             raise Exception("No embeddings returned.")
     else:
-        raise Exception(f"Ollama error: {response.text}")
+        res = f"Ollama error: {response.text}"
+        chat_logger.debug (res)
+        chat_logger.error (res)
+        raise Exception(res)
