de45fa25f30cd920e01c59c46cd5ae0675a16a90
VGiktara27 <curiousvidushi27@gmail.com>
2025-04-27 21:39:14 +0530

    Update Common.py

--- chatbot/src/Common.py
+++ chatbot/src/Common.py
@@ -11,11 +11,18 @@
 '''
 
 # Import Modules
-from pathlib import Path
 import os
 import yaml
+import logging
+from pathlib import Path
+
+'''***************************************** Main Code ********************************************'''
+# Adding app_logger
+chat_logger = logging.getLogger('app_logger')
+
 
 def _read_chatbot_config():
+    chat_logger.info("_read_chatbot_config")
     config_path = os.path.join(Path(__file__).resolve().parent.parent.parent, 'config')
     with open(config_path + '/config.yaml') as fh:
         chat_config = yaml.load(fh, Loader=yaml.FullLoader)
@@ -23,20 +30,24 @@ def _read_chatbot_config():
 
 
 def chat_bot_config():
+    chat_logger.info("chat_bot_config")
     chat_config = _read_chatbot_config()
     return chat_config['ChatBot']
 
 
 def portfolio_config():
+    chat_logger.info("portfolio_config")
     chat_config = _read_chatbot_config()
     return chat_config['Portfolio']
 
 
 def model_config():
+    chat_logger.info("model_config")
     chat_config = _read_chatbot_config()
     return chat_config['AIModel'] 
 
 
 def db_config():
+    chat_logger.info("db_config")
     chat_config = _read_chatbot_config()
     return chat_config['DBConfig'] 
